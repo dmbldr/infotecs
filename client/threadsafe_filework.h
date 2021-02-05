988c9332e85645375b8ecbac6ea72cef10ff08d7
dmbldr <ser.bobcko51@gmail.com>
2021-02-05 11:46:48 +0300

    Add exceptions

--- client/threadsafe_filework.h
+++ client/threadsafe_filework.h
@@ -11,13 +11,11 @@
 
 class threadsafe_filework {
 public:
-    explicit threadsafe_filework(std::string file_name) : _file_name(std::move(file_name)) {};
+    explicit threadsafe_filework(std::string file_name = "buffer.txt") : _file_name(std::move(file_name)) {};
 
     void push(const std::string& msg);
     std::string wait_and_pop();
     bool empty() const;
-
-    //TODO: какие операции стоит запретить
 
     threadsafe_filework(const threadsafe_filework&) = delete;
     threadsafe_filework(threadsafe_filework&&) = delete;
